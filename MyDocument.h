6b7fa73516557d6b2514dec57a19c7c7708ebd2e
beepscore <support@beepscore.com>
2009-11-12 23:40:34 -0800

    Add comment reference Apple Documentation Cocoa Bindings Programming Topics / Providing Controller Content  / Setting the Content of a Controller

--- MyDocument.h
+++ MyDocument.h
@@ -8,6 +8,9 @@
 //  UW CP-120 Certificate in iPhone and Cocoa Development
 //  Q1 Intro to Programming in Objective-C and the Cocoa Framework.
 //  Homework assignment 6
+//  Ref Cocoa Bindings Programming Topics / Providing Controller Content
+//        / Setting the Content of a Controller
+// http://developer.apple.com/Mac/library/documentation/Cocoa/Conceptual/CocoaBindings/Concepts/CntrlContent.html
 
 
 #import <Cocoa/Cocoa.h>

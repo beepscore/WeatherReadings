9fe1a4abb51905701588b76f49c11fa0abe2eb25
beepscore <support@beepscore.com>
2009-11-24 11:43:05 -0800

    Add BSWebViewController.h, .m.  Display open street map, hardcoded to KBFI lat, long.
    TODO: redraw based on selected weather station.

---
+++ BSWebViewController.h
@@ -0,0 +1,17 @@
+//
+//  BSWebViewController.h
+//  WeatherReadings
+//
+//  Created by Steve Baker on 11/24/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+@class WebView;
+
+@interface BSWebViewController : NSView {
+    
+    IBOutlet WebView *myWebView;
+}
+
+@end
---
+++ BSWebViewController.m
@@ -0,0 +1,54 @@
+//
+//  BSWebViewController.m
+//  WeatherReadings
+//
+//  Created by Steve Baker on 11/24/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import "BSWebViewController.h"
+#import <WebKit/WebKit.h>
+
+
+@implementation BSWebViewController
+
+#pragma mark Methods Xcode generated for NSView
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+#pragma mark sbMethods
+- (void) awakeFromNib {
+    
+    // Set an initial URL on the web view so we'll have something to display.
+    // google maps free to use on free web sites.
+    // [myWebView setMainFrameURL: @"http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=weather+station+KBFI"];
+    
+    // open street map
+    [myWebView setMainFrameURL: @"http://www.openstreetmap.org/?lat=47.530000&lon=-122.301972&zoom=12"];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+#pragma mark -
+- (void)dealloc {
+    
+    // after release, best practice is set object = nil;
+    // then if someone accidentally calls it,
+    // they will get nil instead of a bad reference.
+    // setting to nil will cause release also.
+    
+    [super dealloc];
+}
+
+@end
